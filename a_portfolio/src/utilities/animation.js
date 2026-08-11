907d8f4d9137d9a65d67b6d3b6b91c0d7cef27e9
alpie_sahai <168612863+alpasahai@users.noreply.github.com>
2026-08-11 15:11:22 +1000

    Adding the animations

---
+++ a_portfolio/src/utilities/animation.js
@@ -0,0 +1,41 @@
+//This is for all my little animations:
+
+//Succession animation - "When the page initially opens, the main title and icons grow in"
+export const succession_container = {
+    hidden: {},
+    visible: {
+        transition: {
+            staggerChildren: 0.15, //delay between the icons popping in
+        },
+    },
+};
+
+export const grow_in_item = {
+    hidden: {opacity: 0, scale: 0},
+    visible: {
+        opacity: 1,
+        scale: 1,
+        transition: {type: "spring", stiffness: 120, damping: 10},
+    },
+};
+
+//Shimmy-situation - gives a little wiggle for hand-drawn effect
+export const shimmy = {
+    whileHover: {
+        rotate: [0, -6, 6, -4, 4, 0], 
+        transition: {duration: 0.5},
+    },
+};
+
+//Panning from RIGHT
+export const pan_right = {
+    hidden: {opacity: 0, x: 100},
+    visible: {opacity: 1, x: 0, transition: {duration: 0.7, ease: "easeOut"}},
+};
+
+//Panning from LEFT
+export const pan_left = {
+    hidden: {opacity: 0, x: -100},
+    visible: {opacity: 1, x: 0, transition: {duration: 0.7, ease: "easeOut"}},
+};
+
