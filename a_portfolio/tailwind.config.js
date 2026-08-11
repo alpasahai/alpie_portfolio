20271a3adc76408a38409001a8c7d84a58b9ddb2
alpie_sahai <168612863+alpasahai@users.noreply.github.com>
2026-08-11 15:00:34 +1000

    Configuring tailwind

--- a_portfolio/tailwind.config.js
+++ a_portfolio/tailwind.config.js
@@ -1,9 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        maroon: '#4B0A13',   //background
+        rose: '#B14B61',     //main elements + fonts
+        blush: '#D39AA6',    //accents
+        creme: '#D9D9D9',    //paper and polaroids
+      },
+      fontFamily: {
+        nav: ['Italiana', 'serif'],
+        body: ['Inika', 'serif'],
+        heading: ['Freehand', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
-
